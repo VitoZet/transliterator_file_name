3b0e1e495675900a083ecad360afa7c51949f5f8
VitoZet <slava@gigel.ru>
2017-11-15 22:55:42 +0300

    'PyQt'

--- transliterator.py
+++ transliterator.py
@@ -1,12 +1,34 @@
+import sys
 from os import rename, listdir
 from os.path import basename, splitext
 from pytils import translit
-path = input("Введите адресс папки:")
+from PyQt5.QtWidgets import (QApplication, QMainWindow)
+from PyQt5.uic import loadUi
 
-list_file = listdir(path)
 
-for n in list_file:
-    transl_file_name = translit.slugify(splitext(basename(n))[0])
-    extension_file = splitext(n)[1]
-    rename(path+'\\' + n,path+'\\' + transl_file_name + extension_file)
+class Transliterator(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.initUi()
+        self.initSignal()
 
+    def initUi(self):
+        loadUi('ui_transliterator.ui', self)
+
+    def initSignal(self):
+        self.startButton.clicked.connect(self.work_transliterator)
+
+    def work_transliterator(self):
+        path = self.directory.toPlainText()
+        list_file = listdir(path)
+        for n in list_file:
+            transl_file_name = translit.slugify(splitext(basename(n))[0])
+            extension_file = splitext(n)[1]
+            rename(path + '\\' + n, path + '\\' + transl_file_name + extension_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    Transliterator = Transliterator()
+    Transliterator.show()
+    sys.exit(app.exec_())
